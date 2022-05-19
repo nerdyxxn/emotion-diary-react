443b72f82544182fdc43dfc27f8f79f7b1e5a81e
윤성 <yunyul1209@gmail.com>
2022-05-19 15:42:32 +0900

    feat(Router) : Use the three features of React Router V6
    React Router V6의 세 가지 기능 사용하기 (useParams, useSearchParams, useNavigate)

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,30 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log(id);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
